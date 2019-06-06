993127cf8e2677ccd99528f3ddf03e1b423e46d0
iAnafem <sidviciouc@gmail.com>
2019-06-06 19:20:38 +0300

    Before the challenge in the part 6

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -29,3 +30,15 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+

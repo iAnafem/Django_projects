d1cf2b2cde7989a902e9bdee334b2677886f22b2
iAnafem <sidviciouc@gmail.com>
2019-06-22 23:45:10 +0300

    Chapter 8 complete!

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 
 def index(request):
@@ -63,3 +64,14 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class LoanedBooksByAllUsersListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to all users. Only visible for librarians"""
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all_users.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
